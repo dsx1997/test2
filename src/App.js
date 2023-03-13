031a230901e33d09d3cdca7aab758f9118b89d36
dsx1997 <threebigstar@outlook.com>
2023-03-13 17:04:34 +0900

    Declaring a winner

--- src/App.js
+++ src/App.js
@@ -51,7 +51,12 @@ class Board extends React.Component {
     const buff = this.state.squares.slice();
     console.log('buff');
     console.log(buff);
+    if(buff[i]) {
+      return;
+    }
     buff[i] = (this.state.xIsNext ? 'X' : 'O');
+
+
     this.setState({
       squares : buff,
       xIsNext : !this.state.xIsNext,
@@ -63,7 +68,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+
+    let winner = judgeWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = 'Winner is : ' + winner;
+    } else {
+      status = 'Next player: X';
+    }
 
     return (
       <div>
@@ -104,6 +116,25 @@ class Game extends React.Component {
   }
 }
 
+function judgeWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    let [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 
